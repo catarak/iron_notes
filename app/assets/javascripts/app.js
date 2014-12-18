0244017be97778e4064c42b253d4a80da3eb9b1e
catarak <ctarakajian@gmail.com>
2014-12-18 15:11:00 -0500

    lecture notes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -13,3 +13,54 @@ IronNotes.NotesRoute = Ember.Route.extend({
     return this.store.find('note');
   }
 })
+
+IronNotes.NoteController = Ember.ObjectController.extend({
+  actions: {
+    editing: false,
+    actions: {
+      startEdit: function() {
+        this.set('editing', true)
+      },
+      saveNote: function() {
+        var note = this.get('model')
+        var controller = this;
+        note.save().then(function() {
+          controller.set('editing', false)
+        })
+      },
+      deleteNote: function() {
+        var note = this.get('model')
+        var controller = this;
+        note.destroyRecord().then(function() {
+          controller.transitionToRoute('notes')
+        })
+      }
+    }
+  }
+})
+
+IronNotes.NotesIndexController = Ember.ArrayController.extend({
+  newTitle: '',
+  newBody: '',
+  newPriorityLevel: '',
+  actions: {
+    createNote: function() {
+      var title = this.get('newTitle')
+      var body = this.get('newBody')
+      var priorityLevel = this.get('newPriorityLevel')
+
+      var note = this.store.createRecord('note', {
+        title: title,
+        body: body,
+        priorityLevel: priorityLevel
+      })
+      var controller = this;
+      note.save().then(function(model) {
+        controller.set('newTitle', '');
+        controller.set('newBody', '');
+        controller.set('newPriorityLevel', '');
+        controller.transitionToRoute('note', model)
+      })
+    }
+  }
+})
